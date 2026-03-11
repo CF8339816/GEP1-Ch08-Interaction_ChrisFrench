2e668f61162690269cc3cfaedef8d5cdc685f0a3
CF8339816 <chris_french_3dga@hotmail.com>
2026-03-11 00:38:11 -0300

    started to write a clear message method need  to connect it to interface

--- Assets/Systems/UIManager.cs
+++ Assets/Systems/UIManager.cs
@@ -7,11 +7,27 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] bool debugEnabled = false;
     [SerializeField] private TMP_Text messageText;
-    private string currentMessage = "";    
+    private string currentMessage = "";
+
+    private void Start()
+    {
+        currentMessage = "";
+       
+    }
+
 
     public void DisplayMessage(string message)
     {
         currentMessage = message;
+        messageText.text = currentMessage;
+
+
+
+    }
+
+    public void ClearMessage(string message)
+    {
+        currentMessage = "";
         messageText.text = currentMessage;
 
 
